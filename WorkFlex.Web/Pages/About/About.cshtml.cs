525a618a6d68ea901ffa86fd39540d6e1ec02d0f
NguyenXuanQuy <nguyenxuanquy147@gmail.com>
2024-10-06 16:27:14 +0700

    feat:Add Initial UI for WorkFlex.Web
    - Before: Do not have any UI for the whole web project
    - After: Add Initial UI for the web project

---
+++ WorkFlex.Web/Pages/About/About.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WorkFlex.Web.Pages.About
+{
+    public class AboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
