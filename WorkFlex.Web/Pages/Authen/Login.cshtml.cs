875c8ee1ed77b2a576267a9206c64c634f56857f
NguyenXuanQuy <nguyenxuanquy147@gmail.com>
2024-10-07 10:48:58 +0700

    fix:Fix bug scan pull merge request

--- WorkFlex.Web/Pages/Authen/Login.cshtml.cs
+++ WorkFlex.Web/Pages/Authen/Login.cshtml.cs
@@ -1,4 +1,3 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace WorkFlex.Web.Pages.Authen
@@ -7,6 +6,7 @@ namespace WorkFlex.Web.Pages.Authen
     {
         public void OnGet()
         {
+            throw new NotImplementedException();
         }
     }
 }
