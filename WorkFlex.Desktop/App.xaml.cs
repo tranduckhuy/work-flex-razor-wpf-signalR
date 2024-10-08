183b3f62043e46064cd0921787c7e5dedf5e0bf1
Minh Thuận <ndmthuanwork97@gmail.com>
2024-10-08 12:26:02 +0700

    [Feature] Add WPF Login Feature

--- WorkFlex.Desktop/App.xaml.cs
+++ WorkFlex.Desktop/App.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System.Configuration;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Configuration;
 using System.Data;
 using System.Windows;
+using WorkFlex.Desktop.DataAccess.Repositories;
 
 namespace WorkFlex.Desktop
 {
@@ -9,6 +11,26 @@ namespace WorkFlex.Desktop
     /// </summary>
     public partial class App : Application
     {
-    }
+		private IServiceProvider _serviceProvider = null!;
+		public App()
+		{
+			ServiceCollection serviceCollection = new ServiceCollection();
+			ConfigureServices(serviceCollection);
+			_serviceProvider = serviceCollection.BuildServiceProvider();
+		}
+
+		private static void ConfigureServices(ServiceCollection serviceCollection)
+		{
+			serviceCollection.AddSingleton<IUserRepository, UserRepository>();
+			serviceCollection.AddTransient<Login>();
+			serviceCollection.AddTransient<MainWindow>();
+		}
+
+		protected void OnStartup(object sender, StartupEventArgs e)
+		{
+			var login = _serviceProvider.GetRequiredService<Login>();
+			login.Show();
+		}
+	}
 
 }
---
+++ WorkFlex.Desktop/BusinessObject/UserSession.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkFlex.Desktop.BusinessObject
+{
+	public class UserSession
+	{
+		public static UserSession _instance = null;
+		private static readonly object _lock = new object();
+		public UserObject CurrentUser { get; private set; } = null!;
+		public static UserSession Instance
+		{
+			get
+			{
+				lock (_lock)
+				{
+					return _instance ??= new UserSession();
+				}
+			}
+		}
+
+		public void SetUser(UserObject user)
+		{
+			CurrentUser = user;
+		}
+		public UserObject GetUser()
+		{
+			return CurrentUser;
+		}
+		public void Reset()
+		{
+			CurrentUser = null!;
+		}
+	}
+}
