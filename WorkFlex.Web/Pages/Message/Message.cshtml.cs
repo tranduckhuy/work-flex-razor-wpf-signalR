1602c8900a65f4df527038a57f79487ff77f912f
NguyenXuanQuy <nguyenxuanquy147@gmail.com>
2024-10-12 00:51:51 +0700

    feat:Add UI for pages: Dashboard and Message
    - Before: Do not have UI for pages: Dashboard and Message
    - After: Add 2 new UI for pages: Dashboard and Message

---
+++ WorkFlex.Web/Pages/Message/Message.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WorkFlex.Web.Pages.Message
+{
+    public class MessageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ WorkFlex.Web/Repository/Interface/IJobRepository.cs
@@ -0,0 +1,12 @@
+﻿using WorkFlex.Domain.Entities;
+using WorkFlex.Web.ViewModels;
+
+namespace WorkFlex.Web.Repository.Interface
+{
+    public interface IJobRepository
+    {
+        Task<(IEnumerable<JobPost> Jobs, int TotalCount)> GetJobsAsync(JobListVM filters);
+        
+        Task<IEnumerable<JobType>> GetJobTypesAsync();
+    }
+}
