fe8bd5ebad920196516260ba9d328fa06b61719a
Trần Đức Huy <duchuy81.1610@gmail.com>
2024-10-13 10:07:08 +0700

    [Refactor] User entity properties and regenerate migration

--- WorkFlex.Domain/Entities/User.cs
+++ WorkFlex.Domain/Entities/User.cs
@@ -9,7 +9,7 @@ namespace WorkFlex.Domain.Entities
         public Guid Id { get; set; } = Guid.NewGuid();
 
         [StringLength(40)]
-        public string? Username { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
 
         [Required, StringLength(255)]
         public string Password { get; set; } = string.Empty;
@@ -21,7 +21,7 @@ namespace WorkFlex.Domain.Entities
         public string LastName { get; set; } = string.Empty;
 
         [StringLength(255)]
-        public string? Avatar { get; set; } = string.Empty;
+        public string Avatar { get; set; } = string.Empty;
 
         [StringLength(15)]
         public string? Phone { get; set; } = string.Empty;
