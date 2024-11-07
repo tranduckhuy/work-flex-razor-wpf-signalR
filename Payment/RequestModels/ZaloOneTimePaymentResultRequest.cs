bd20c6bef836f00e1eabe0fa90f53366032205c4
NguyenXuanQuy <nguyenxuanquy147@gmail.com>
2024-11-08 02:40:48 +0700

    fix:Fix bug sonar scan

--- Payment/RequestModels/ZaloOneTimePaymentResultRequest.cs
+++ Payment/RequestModels/ZaloOneTimePaymentResultRequest.cs
@@ -2,13 +2,13 @@
 {
     public class ZaloOneTimePaymentResultRequest
     {
-        public long Amount { get; set; }
-        public int AppId { get; set; }
+        public long Amount { get; set; } = 0;
+        public int AppId { get; set; } = 2554;
         public string AppTransId { get; set; } = string.Empty;
         public string CheckSum { get; set; } = string.Empty;
-        public long DiscountAmount { get; set; }
-        public int PmCid { get; set; }
-        public int Status { get; set; }
+        public long DiscountAmount { get; set; } = 0;
+        public int PmCid { get; set; } = 0;
+        public int Status { get; set; } = 0;
         public string Description { get; set; } = string.Empty;
     }
 }
