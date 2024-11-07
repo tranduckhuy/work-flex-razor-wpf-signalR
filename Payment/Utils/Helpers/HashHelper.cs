3808349e56ec0e9d1fd0116f25d97f98db1a31a4
SangTN5 <tranngocsang147877@gmail.com>
2024-11-07 22:31:22 +0700

    feature: add feature vnpay

--- Payment/Utils/Helpers/HashHelper.cs
+++ Payment/Utils/Helpers/HashHelper.cs
@@ -14,5 +14,22 @@ namespace WorkFlex.Payment.Utils.Helpers
                 return BitConverter.ToString(hashBytes).Replace("-", string.Empty).ToLower();
             }
         }
+
+        public static string HmacSHA512(string data, string key)
+        {
+            var hash = new StringBuilder();
+            byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+            byte[] dataBytes = Encoding.UTF8.GetBytes(data);
+            using (var hmacSha512 = new System.Security.Cryptography.HMACSHA512(keyBytes))
+            {
+                byte[] hashBytes = hmacSha512.ComputeHash(dataBytes);
+                foreach (var theByte in hashBytes)
+                {
+                    hash.Append(theByte.ToString("x2"));
+                }
+            }
+            return hash.ToString();
+        }
+
     }
 }
